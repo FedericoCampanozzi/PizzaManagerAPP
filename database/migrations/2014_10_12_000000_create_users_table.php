980f682f270c085ce2f65631f1a627ed827761aa
Federico Campanozzi <federico.campanozzi@gmail.com>
2023-11-07 18:31:32 +0100

    try to implement calendar and charts

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->timestamps('holidays_start')->nullable();
+            $table->timestamps('holidays_end')->nullable();
             $table->integer('fk_role')->unsigned()->default(4)->foreign('fk_role')->references('id')->on('role');
         });
     }
--- resources/js/Pages/Dashboards/Admin.jsx
+++ resources/js/Pages/Dashboards/Admin.jsx
@@ -1,7 +1,73 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import Table from "@/Components/Table.jsx";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from "@fullcalendar/interaction";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const events = [
+  { title: 'Meeting', start: new Date() }
+]
 
 export default function AdminDashboard({ auth, users }) {
+   
+ const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+  
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+   const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [1,2,3,45,6,7,8],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: [1,2,3,45,6,7,8],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+    
+    const handleDateClick = (arg) => { // bind with an arrow function
+        alert(arg.dateStr);
+    }
+    const eventClick = (arg) => { // bind with an arrow function
+        console.log(arg);
+        alert(arg);
+    }
     return (
         <>
             <AuthenticatedLayout
@@ -20,10 +86,68 @@ export default function AdminDashboard({ auth, users }) {
                                     columns={['name','email','userole']} 
                                     action="editrole"
                                     actionlabel='Edit Role'/>
-                        </div>
+                        </div>                        
                     </div>
                 </div>
+
+                <div className="py-12">
+                    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div className='p-5' style={{
+                                    width:'50%',
+                                    float:'left',
+                                    height:'auto'}}>
+                                <Bar options={options} data={data} />
+                            </div>
+                            <div className='p-5' style={{
+                                    width:'50%',
+                                    float:'right',
+                                    height:'auto'}}>
+                                <Bar options={options} data={data} />
+                            </div>
+                            <div className='p-5' style={{
+                                    width:'50%',
+                                    float:'left',
+                                    height:'auto'}}>
+                                <Bar options={options} data={data} />
+                            </div>
+                            <div className='p-5' style={{
+                                    width:'50%',
+                                    float:'right',
+                                    height:'auto'}}>
+                                <Bar options={options} data={data} />
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+                
+                <div className="py-12">
+                    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div className='p-10'>
+                                <FullCalendar
+                                    plugins={[dayGridPlugin, interactionPlugin]}
+                                    initialView='dayGridMonth'
+                                    weekends={true}
+                                    events={events}
+                                    eventClick={eventClick}
+                                    dateClick={handleDateClick}
+                                    eventContent={renderEventContent}/>
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+
             </AuthenticatedLayout>
         </>
     );
+}
+
+function renderEventContent(eventInfo) {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    )
 }
